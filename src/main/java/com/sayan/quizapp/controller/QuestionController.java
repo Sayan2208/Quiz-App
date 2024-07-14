f9ca1b6d94837fd5062bcac919514d53f50aaa11
Sayan Datta <dattasayan825@gmail.com>
2024-07-15 05:29:04 +0530

    Initial Commit

---
+++ src/main/java/com/sayan/quizapp/controller/QuestionController.java
@@ -0,0 +1,36 @@
+package com.sayan.quizapp.controller;
+
+import com.sayan.quizapp.model.Question;
+import com.sayan.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+    @GetMapping("allQuestions")
+    public ResponseEntity<List<Question>> getAllQuestions(){
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
+        return questionService.addQuestion(question);
+    }
+
+    @DeleteMapping("delete")
+    public ResponseEntity<String> deleteQuestion(@RequestBody Question question){
+        return questionService.deleteQuestion(question);
+    }
+}
---
+++ src/main/java/com/sayan/quizapp/model/Response.java
@@ -0,0 +1,11 @@
+package com.sayan.quizapp.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Response {
+    private int id;
+    private String response;
+}
